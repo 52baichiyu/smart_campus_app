222dbfd734cda91eec99022c89f485aee384f9f5
baichi_yu <Puppeteer@qq.com>
2023-11-07 11:22:21 +0800

    The Create Project

---
+++ src/main/java/com/example/smart_campus_app/SmartCampusAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_campus_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCampusAppApplication.class, args);
+    }
+
+}
